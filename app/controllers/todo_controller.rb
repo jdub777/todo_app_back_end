c16bf725d5659551c0dbfe40c89a4f06aff49f4c
Jonathan Mesa <jmesa@cps.edu>
2018-04-16 18:14:24 +0000

    Add root routv2e

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,7 @@
 class TodoController < ApplicationController
-    def index
-      @todos = Todo.all
-    end
+     def index
+     @todos = Todo.all
+     end
     def show
        @todo = Todo.find_by_id(params[:id])
     end
